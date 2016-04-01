8031452dda3eb36bad1a8d467dfc1a0f85ebc408
CAD Client <CAD Client>
2016-04-01 14:43:36 +0900

    ex05 text enter

---
+++ ex05/ex05.py
@@ -0,0 +1,24 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+inch_to_cm= 2.54/1.0
+height_cm = height * inch_to_cm
+
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+print "my heghit is %d tall" % height_cm
+
+
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
