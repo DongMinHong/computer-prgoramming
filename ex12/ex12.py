e14e090feddb82b0820f69562a78f0057ed737ea
CAD Client <CAD Client>
2016-04-08 15:33:34 +0900

    ex13

--- ex12/ex12.py
+++ ex12/ex12.py
@@ -3,5 +3,5 @@ age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 name = raw_input("What is your name number? ")
-print "So, you're %r old, %r tall and %r heavy." % (
+print "So, you're %r old, %r tall and %r heavy %r number." % (
     age, height, weight, name)
